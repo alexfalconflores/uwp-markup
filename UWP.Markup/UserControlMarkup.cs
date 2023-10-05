77b48229590dfa9aa8dc8ea5f7e2ef6be2942ad9
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-10-05 02:25:19 -0500

    ➕ [ADD] UserControlMarkup

---
+++ UWP.Markup/UserControlMarkup.cs
@@ -0,0 +1,21 @@
+﻿
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace UWP.Markup;
+
+public static class UserControlMarkup
+{
+    /// <summary>
+    /// Sets the content that is contained within a user control.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="element"></param>
+    /// <param name="content"></param>
+    /// <returns>The <see cref="UserControl"/> instance for method chaining</returns>
+    public static T Content<T>(this T element, UIElement content) where T : UserControl
+    {
+        element.Content = content;
+        return element;
+    }
+}
