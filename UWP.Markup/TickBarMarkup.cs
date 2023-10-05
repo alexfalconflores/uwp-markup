81710c15fd090074398aedd60764c2d02ecb8e11
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2023-10-05 02:25:53 -0500

    ➕ [ADD] TickBarMarkup

---
+++ UWP.Markup/TickBarMarkup.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace UWP.Markup;
+
+/// <summary>
+/// Represents a tick mark on a Slider control. Not intended for general use.
+/// </summary>
+public static class TickBarMarkup
+{
+    /// <summary>
+    /// Sets the Brush that draws on the background area of the TickBar.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <param name="value"></param>
+    /// <returns>The <see cref="TickBar"/> instance for method chaining</returns>
+    public static TickBar Fill(this TickBar element, Brush value)
+    {
+        element.Fill = value;
+        return element;
+    }
+}
